96a0dfa676ef6f1e8a08d1222b326514cc6f0cca
Mateus Antunes <mateus.antunes95@gmail.com>
2025-06-20 13:15:37 -0300

    add model address

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Address extends Model
 {
-    //
+    protected $fillable = ['user_id', 'cep', 'logradouro', 'bairro', 'cidade', 'estado'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
