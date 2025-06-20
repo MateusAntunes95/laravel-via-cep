3a9a27ceef64094a6bdec03b66cd3563b6ecabb5
Mateus Antunes <mateus.antunes95@gmail.com>
2025-06-20 13:35:28 -0300

    add UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Http;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        return User::with('address')->get();
+    }
+
+    public function show($id)
+    {
+        return User::with('address')->findOrFail($id);
+    }
+
+    public function store(UserRequest $request)
+    {
+        $cep = preg_replace('/[^0-9]/', '', $request->cep);
+        $response = Http::get("https://viacep.com.br/ws/{$cep}/json/");
+
+        if ($response->json('erro')) {
+            return response()->json(['message' => 'CEP inválido'], 422);
+        }
+
+        $user = User::create($request->only(['name', 'email']));
+
+        $user->address()->create([
+            'cep' => $cep,
+            'logradouro' => $response['logradouro'],
+            'bairro' => $response['bairro'],
+            'cidade' => $response['localidade'],
+            'estado' => $response['uf'],
+        ]);
+
+        return response()->json($user->load('address'), 201);
+    }
 }
